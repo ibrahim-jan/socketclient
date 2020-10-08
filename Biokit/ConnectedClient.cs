0ce7eff54c234a1a23d4ce546a644a09d6c8f3d5
ibrahim <ibrahim.bsit@gmail.com>
2020-10-08 16:14:07 +0300

    initial

---
+++ Biokit/ConnectedClient.cs
@@ -0,0 +1,12 @@
+﻿namespace Biokit
+{
+    public class ConnectedClient
+    {
+        public string ConnectionId { get; set; }
+
+        public string AppName { get; set; }
+
+        public string AppVersion { get; set; }
+         
+    }
+}
---
+++ Biokit/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace Biokit
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Press a key to start listening..");
+            Console.ReadKey();
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44367/applicationshub")
+                .Build();
+
+            var clientInfo = new ConnectedClient
+            {
+                AppName = "Biokit",
+                AppVersion = "1.0"
+            };
+
+            connection.On<ConnectedClient>("UpdateClientInfo", (client) =>
+            {
+                clientInfo.ConnectionId = client.ConnectionId;
+                Console.WriteLine(clientInfo);
+            });
+
+            connection.StartAsync().GetAwaiter().GetResult();
+
+            connection.InvokeAsync("GetAppInfo", clientInfo).GetAwaiter().GetResult();
+
+            Console.WriteLine("Listening..... Press a key to quit");
+            Console.ReadKey();
+        }
+    }
+}
