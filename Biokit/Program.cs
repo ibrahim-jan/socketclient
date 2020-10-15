cefed98b733fd95b826b66d677b45772bdc231c0
ibrahim <ibrahim.bsit@gmail.com>
2020-10-15 14:07:08 +0300

    removing ssl

--- Biokit/Program.cs
+++ Biokit/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Net.Http;
 
 namespace Biokit
 {
@@ -12,31 +13,48 @@ namespace Biokit
             Console.WriteLine("Press a key to start listening..");
             Console.ReadKey();
 
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json");
-
-            var configuration = builder.Build();
-
-            var url = configuration["hostUrl"];
-            var connection = new HubConnectionBuilder()
-                .WithUrl(url)
-                .Build();
-
-            var clientInfo = new ConnectedClient
+            try
             {
-                AppName = "Biokit",
-                AppVersion = "1.0"
-            };
+                var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                       .AddJsonFile("appsettings.json");
 
-            connection.On<ConnectedClient>("UpdateClientInfo", (client) =>
+                var configuration = builder.Build();
+
+                var url = configuration["hostUrl"];
+                var connection = new HubConnectionBuilder()
+                    .WithUrl(url,(opts)=>
+                    {
+                        opts.HttpMessageHandlerFactory = (message) =>
+                         {
+                             if (message is HttpClientHandler clientHandler)
+                                 // bypass SSL certificate
+                                 clientHandler.ServerCertificateCustomValidationCallback +=
+                                     (sender, certificate, chain, sslPolicyErrors) => { return true; };
+                             return message;
+                         };
+                    })
+                    .Build();
+
+                var clientInfo = new ConnectedClient
+                {
+                    AppName = "Biokit",
+                    AppVersion = "1.0"
+                };
+
+                connection.On<ConnectedClient>("UpdateClientInfo", (client) =>
+                {
+                    clientInfo.ConnectionId = client.ConnectionId;
+                    Console.WriteLine(clientInfo);
+                });
+
+                connection.StartAsync().GetAwaiter().GetResult();
+
+                connection.InvokeAsync("GetAppInfo", clientInfo).GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
             {
-                clientInfo.ConnectionId = client.ConnectionId;
-                Console.WriteLine(clientInfo);
-            });
-
-            connection.StartAsync().GetAwaiter().GetResult();
-
-            connection.InvokeAsync("GetAppInfo", clientInfo).GetAwaiter().GetResult();
+                Console.WriteLine(ex.Message);
+            }
 
             Console.WriteLine("Listening..... Press a key to quit");
             Console.ReadKey();
